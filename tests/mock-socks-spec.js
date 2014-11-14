46a9819ce6f4f7057b2ca3ad7912122794620e23
Travis Hoover <thoov7@gmail.com>
2014-11-13 18:01:57 -0800

    Adding the ability to send mock messages

--- tests/mock-socks-spec.js
+++ tests/mock-socks-spec.js
@@ -2,17 +2,23 @@ var protocol = new Protocol();
 var exampleServer = new MockSocksServer(protocol);
 exampleServer.on('connection', function(server) {
 
-	// server.on('message', function(data) {
-	//
-	// 	server.send('testing');
-	//
-	// });
+	server.on('message', function(data) {
+		server.send('hello');
+	});
+
 });
 
-test('basic test', function(){
+asyncTest('basic test', function(){
     var exampleSocket = new MockSocks('ws://www.example.com/socketserver', protocol);
 
 	exampleSocket.onopen(function() {
 		equal(true, true, 'onopen fires as expected');
 	});
+
+	exampleSocket.onmessage(function(data) {
+		equal(true, true, 'onmessage fires as expected');
+		start();
+	});
+
+	exampleSocket.send('world');
 });
