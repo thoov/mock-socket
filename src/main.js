8be4d55236d8ca06c7a223fdd541fc6a7019e128
Travis Hoover <thoov7@gmail.com>
2014-12-07 22:06:50 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// Starting point for browserify and throws important objects into the window object
 var protocol   = require('./protocol');
 var mockserver = require('./mock-server');
 var mocksocket = require('./mock-socket');
