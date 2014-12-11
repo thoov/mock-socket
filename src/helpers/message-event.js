34ad1b043039695fa91e1566e9b464df281df251
Travis Hoover <thoov7@gmail.com>
2014-12-11 11:08:58 -0800

    Adding mock message event for phantomjs

--- src/helpers/message-event.js
+++ src/helpers/message-event.js
@@ -7,27 +7,55 @@
 * @param {origin: string} The url of the place where the event is originating.
 */
 function socketEventMessage(name, data, origin) {
-	var bubbles 		= false;
-	var cancelable 		= false;
-	var lastEventId 	= null;
-	var source			= null;
-	var ports 			= null;
+	var bubbles 				= false;
+	var cancelable 			= false;
+	var lastEventId 		= null;
+	var source					= null;
+	var ports 					= null;
 	var sourcePlacehold = null;
-	var messageEvent 	= new MessageEvent(name);
-	messageEvent.initMessageEvent(name, bubbles, cancelable, data, origin, lastEventId, source, ports);
 
-	Object.defineProperties(messageEvent, {
-		source:  {
-			get: function() { return sourcePlacehold; },
-			set: function(value) { sourcePlacehold = value; }
-		},
-		srcElement: {
-			get: function() { return messageEvent.source; }
-		},
-		currentTarget: {
-			get: function() { return messageEvent.source; }
-		}
-	});
+	try {
+		var messageEvent = new MessageEvent(name);
+		messageEvent.initMessageEvent(name, bubbles, cancelable, data, origin, lastEventId, source, ports);
+
+		Object.defineProperties(messageEvent, {
+			source:  {
+				get: function() { return sourcePlacehold; },
+				set: function(value) { sourcePlacehold = value; }
+			},
+			srcElement: {
+				get: function() { return this.source; }
+			},
+			currentTarget: {
+				get: function() { return this.source; }
+			}
+		});
+	}
+	catch (e) {
+		// We are unable to create a MessageEvent Object. This should only be happening in PhantomJS.
+		var messageEvent = {
+			bubbles: bubbles,
+			cancelable: cancelable,
+			data: data,
+			origin: origin,
+			lastEventId: lastEventId,
+			source: source,
+			ports: ports
+		};
+
+		Object.defineProperties(messageEvent, {
+			source:  {
+				get: function() { return sourcePlacehold; },
+				set: function(value) { sourcePlacehold = value; }
+			},
+			srcElement: {
+				get: function() { return this.source; }
+			},
+			currentTarget: {
+				get: function() { return this.source; }
+			}
+		});
+	}
 
 	return messageEvent;
 }
