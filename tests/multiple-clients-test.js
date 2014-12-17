e5c1ab7cf684e52eb23e61162fcddcec5268becd
Travis Hoover <thoov7@gmail.com>
2014-12-17 11:34:18 -0800

    Updating the multiple clients test

--- tests/multiple-clients-test.js
+++ tests/multiple-clients-test.js
@@ -14,11 +14,11 @@ asyncTest('that the onopen function will only be called once for each client', f
   });
 
   socketA.onopen = function() {
-    ok(true, 'mocksocket onclose fires as expected');
+    ok(true, 'mocksocket onopen fires as expected');
   };
 
   socketB.onopen = function() {
-    ok(true, 'mocksocket onclose fires as expected');
+    ok(true, 'mocksocket onopen fires as expected');
     start();
   };
 });
@@ -42,15 +42,14 @@ asyncTest('mock clients will connect to the right mock server', function() {
   });
 
   socketA.onopen = function() {
-    ok(true, 'mocksocket onclose fires as expected');
+    ok(true, 'mocksocket onopen fires as expected');
   };
 
   socketB.onopen = function() {
-    ok(true, 'mocksocket onclose fires as expected');
+    ok(true, 'mocksocket onopen fires as expected');
     start();
   };
 });
-
 
 asyncTest('mock clients can send message to the right mock server', function() {
   var serverA = new MockServer('ws://localhost:8080');
@@ -80,12 +79,40 @@ asyncTest('mock clients can send message to the right mock server', function() {
   });
 
   socketA.onopen = function() {
-    ok(true, 'mocksocket onclose fires as expected');
+    ok(true, 'mocksocket onopen fires as expected');
     this.send(dataA);
   };
 
   socketB.onopen = function() {
-    ok(true, 'mocksocket onclose fires as expected');
+    ok(true, 'mocksocket onopen fires as expected');
     this.send(dataB);
+  };
+});
+
+
+asyncTest('mock clients can send message to the right mock server', function() {
+  var semaphore  = 0;
+  var mockServer = new MockServer('ws://localhost:8080');
+  var socketA    = new MockSocket('ws://localhost:8080');
+  var socketB    = new MockSocket('ws://localhost:8080');
+
+  expect(2);
+
+  mockServer.on('connection', function(server) {
+    semaphore++;
+
+    // Wait for both clients to connect then close the connection.
+    if(semaphore === 2) {
+      server.close();
+    }
+  });
+
+  socketA.onclose = function() {
+    ok(true, 'mocksocket onclose fires as expected');
+  };
+
+  socketB.onclose = function() {
+    ok(true, 'mocksocket onclose fires as expected');
+    start();
   };
 });
