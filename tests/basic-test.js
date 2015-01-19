bd4ea32c0e53c0637b2b1e3d3899db8d346267a8
Pedro Kiefer <pedro@kiefer.com.br>
2015-01-19 17:52:28 -0200

    Remove query params from urls

--- tests/basic-test.js
+++ tests/basic-test.js
@@ -30,3 +30,14 @@ asyncTest('On message with the server happens correctly', function() {
     start();
   };
 });
+
+asyncTest('Connection with the server with query params happens correctly', function() {
+  var mockSocket = new MockSocket('ws://localhost:8080?param1=2&param2=1');
+
+  expect(1);
+
+  mockSocket.onopen = function() {
+    ok(true, 'onopen fires as expected');
+    start();
+  };
+});
