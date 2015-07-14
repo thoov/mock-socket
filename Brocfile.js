36786fdac07034043122be00f8c14b9e9dac5470
Travis Hoover <thoov7@gmail.com>
2015-07-13 22:39:41 -0700

    Updating broc

--- Brocfile.js
+++ Brocfile.js
@@ -54,6 +54,8 @@ var uglyTree = funnel(uglifyJavaScript(browserifiedSrcTree), {
 var testPackage = mergeTrees([
   // Load all of the src files
   funnel('src', { destDir: '/src'}),
+  // Load all vendor files needed
+  funnel('node_modules/qunitjs/qunit', { include: ['qunit.js'], destDir: '/'}),
 
   // Load all of the test files (ie: all files ending with -test.js)
   funnel('tests', { include: ['*-test.js', 'test-loader.js'], destDir: '/'}),
@@ -64,6 +66,7 @@ var testPackage = mergeTrees([
 * Convert all test files to es5 and allow lookups for 3rd party files
 */
 var es5TestTree = esTranspiler(testPackage, {
+  blacklist: ['useStrict'],
   resolveModuleSource: function(source, filepath) {
     switch(source) {
       case 'qunit':
@@ -81,20 +84,10 @@ var es5TestTree = esTranspiler(testPackage, {
 });
 
 /*
-* Collection of all files including test files in es5 and vendor files
-*/
-var es5TestFullTree = mergeTrees([
-  es5TestTree,
-
-  // Load all vendor files needed
-  funnel('node_modules/qunitjs/qunit', { include: ['qunit.js'], destDir: '/'})
-]);
-
-/*
 * Browserify the tests so that they can be run via testem's index.html file. The test-loader's
 * job is to include the src files at the top then to include the test modules.
 */
-var browserifiedTestTree = fastBrowserify(es5TestFullTree, {
+var browserifiedTestTree = fastBrowserify(es5TestTree, {
   bundles: {
     'tests.js': {
       entryPoints: ['test-loader.js']
