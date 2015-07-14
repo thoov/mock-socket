64ca4be5b43ef751e514397cd667f94e2a9eedf8
Travis Hoover <thoov7@gmail.com>
2015-07-14 15:49:08 -0700

    Adding testing playground

---
+++ tests/playground/server.js
@@ -0,0 +1,10 @@
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
