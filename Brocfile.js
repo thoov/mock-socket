d142769b7785426736330afa8f6f2a17bc38507e
Travis Hoover <thoov7@gmail.com>
2015-07-13 19:29:39 -0700

    Improving brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,105 +1,60 @@
-var funnel           = require('broccoli-funnel');
-var uglifyJavaScript = require('broccoli-uglify-js');
-var mergeTrees       = require('broccoli-merge-trees');
-var fastBrowserify   = require('broccoli-fast-browserify');
-var esTranspiler     = require('broccoli-babel-transpiler');
+var funnel            = require('broccoli-funnel');
+var uglifyJavaScript  = require('broccoli-uglify-js');
+var mergeTrees        = require('broccoli-merge-trees');
+var moduleLookupHash  = require('./helpers/module-lookup');
+var fastBrowserify    = require('broccoli-fast-browserify');
+var babelTransform    = require('broccoli-babel-transpiler');
+var pathNormalization = require('./helpers/module-path-normalization');
 
-/*
-* Convert all of the src files into es5 and then browserify them to be
-* used within the browser.
-*/
-var srcVendorPackage = mergeTrees([
-  // Load all of the src files
-  funnel('src', { destDir: '/src'}),
-  funnel('node_modules/URIjs/src', { include: ['URI.min.js', 'punycode.js', 'IPv6.js', 'SecondLevelDomains.js', 'URI.js'], destDir: '/'})
+var thirdPartyLibaries = mergeTrees([
+  funnel('node_modules/URIjs/src', {
+    include: [
+      'URI.min.js',
+      'punycode.js',
+      'IPv6.js',
+      'SecondLevelDomains.js',
+      'URI.js'
+    ],
+    destDir: '/'}),
+
+  funnel('node_modules/qunitjs/qunit', { include: ['qunit.js'], destDir: '/'})
 ]);
-var es5SrcTree = esTranspiler(srcVendorPackage, {
-  resolveModuleSource: function(source, filepath) {
-    switch(source) {
-      case 'urijs':
-        var arrayOfNestedDirs = filepath.split('/');
 
-        if(arrayOfNestedDirs.length === 1) { return './URI.min.js'; }
-
-        // for each nested dir we place a ../ infront of qunit.js
-        return arrayOfNestedDirs.map(function() { return '../'; }).slice(0, -1).join('') + 'URI.min.js';
-
-      default:
-        return source;
-    }
-  }
-});
-var browserifiedSrcTree = fastBrowserify(es5SrcTree, {
-  bundles: {
-    'mock-socket.js': {
-      entryPoints: ['**/main.js']
-    }
-  }
-});
-
-/*
-* Take the browserified file and minify / uglify it.
-*/
-var uglyTree = funnel(uglifyJavaScript(browserifiedSrcTree), {
-  destDir: '/',
-  getDestinationPath: function() {
-    return 'mock-socket.min.js';
-  }
-});
-
-/*
-* TestPackage is a collection of all the needed files for the tests to run. This includes
-* the source files, the test files, and the test loader.
-*/
-var testPackage = mergeTrees([
-  // Load all of the src files
-  funnel('src', { destDir: '/src'}),
-
+var testFiles = mergeTrees([
   // Load all of the test files (ie: all files ending with -test.js)
   funnel('tests', { include: ['*-test.js', 'test-loader.js'], destDir: '/'}),
   funnel('tests/bug-reports', { include: ['*-test.js'], destDir: '/bug-reports'})
 ]);
 
-/*
-* Convert all test files to es5 and allow lookups for 3rd party files
-*/
-var es5TestTree = esTranspiler(testPackage, {
-  resolveModuleSource: function(source, filepath) {
-    switch(source) {
-      case 'qunit':
-        var arrayOfNestedDirs = filepath.split('/');
-
-        if(arrayOfNestedDirs.length === 1) { return './qunit.js'; }
-
-        // for each nested dir we place a ../ infront of qunit.js
-        return arrayOfNestedDirs.map(function() { return '../'; }).slice(0, -1).join('') + 'qunit.js';
-
-      default:
-        return source;
-    }
-  }
-});
-
-/*
-* Collection of all files including test files in es5 and vendor files
-*/
-var es5TestFullTree = mergeTrees([
-  es5TestTree,
-
-  // Load all vendor files needed
-  funnel('node_modules/qunitjs/qunit', { include: ['qunit.js'], destDir: '/'})
+var completeTree = mergeTrees([
+  thirdPartyLibaries,
+  funnel('src', { destDir: '/src'}),
+  testFiles
 ]);
 
-/*
-* Browserify the tests so that they can be run via testem's index.html file. The test-loader's
-* job is to include the src files at the top then to include the test modules.
-*/
-var browserifiedTestTree = fastBrowserify(es5TestFullTree, {
-  bundles: {
-    'tests.js': {
-      entryPoints: ['test-loader.js']
+var es5Tree = babelTransform(completeTree, {
+  blacklist: ['useStrict'],
+  resolveModuleSource: function(source, filepath) {
+    if(moduleLookupHash[source]) {
+      return pathNormalization(filepath) + moduleLookupHash[source].filename;
     }
+
+    return source;
   }
 });
 
-module.exports = mergeTrees([browserifiedSrcTree, uglyTree, browserifiedTestTree]);
+var browserifiedTree = fastBrowserify(es5Tree, {
+  bundles: {
+    'mock-socket.js': { entryPoints: ['**/main.js'] },
+    'tests.js': { entryPoints: ['test-loader.js'] }
+  }
+});
+
+var minifiedTree = uglifyJavaScript(funnel(browserifiedTree, {
+  include: ['mock-socket.js'],
+  getDestinationPath: function() {
+    return 'mock-socket.min.js';
+  }
+}));
+
+module.exports = mergeTrees([browserifiedTree, minifiedTree]);
---
+++ helpers/module-lookup.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'urijs': {
+    'filename': 'URI.min.js'
+  },
+  'qunit': {
+    'filename': 'qunit.js'
+  }
+};
