8c3d1426701215dc739426c2f8f424c00a3ba923
Travis Hoover <thoov7@gmail.com>
2016-11-05 11:02:14 -0700

    UMD support (#129)
    Add proper UMD support via webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    library: 'Mock',
+    libraryTarget: 'umd',
+    filename: 'dist/mock-socket.js',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  }
+};
