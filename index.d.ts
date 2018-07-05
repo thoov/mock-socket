841dcf84e30de0f12b57c92d015370b2cf7a62d4
Travis Hoover <thoov7@gmail.com>
2018-07-05 11:51:47 -0700

    [FEAT #177] Add typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,72 @@
+// Type definitions for Mock Socket 8.X+
+// Project: Mock Socket
+// Definitions by: Travis Hoover <https://github.com/thoov/mock-socket>
+
+declare module 'mock-socket' {
+  //
+  // https://html.spec.whatwg.org/multipage/web-sockets.html#websocket
+  //
+  class WebSocket extends EventTarget {
+    constructor(url?: USVString, protocols?: string|string[]);
+
+    readonly url: USVString;
+
+    readonly CONNECTING: 0;
+    readonly OPEN: 1;
+    readonly CLOSING: 2;
+    readonly CLOSED: 3;
+    readonly readState: number;
+    readonly bufferedAmount: number;
+
+    onopen: EventHandlerNonNull;
+    onerror: EventHandlerNonNull;
+    onclose: EventHandlerNonNull;
+    readonly extensions: string;
+    readonly protocol: string;
+    close(code?: number, reason?: string): void;
+
+    onmessage: EventHandlerNonNull;
+    binaryType: BinaryType;
+    send(data: USVString | Blob | ArrayBuffer | ArrayBufferView): void;
+  }
+
+  class Server extends EventTarget {
+    constructor(url: USVString, options?: ServerOptions);
+
+    readonly options?: ServerOptions;
+
+    start(): void;
+    stop(callback: VoidFunction): void;
+
+    on(type: string, callback: VoidFunction): void;
+    close(options: CloseOptions): void;
+    emit(event: Event, data: any, options: EmitOptions): void;
+
+    clients(): WebSocket[];
+    to(room: any, broadcaster: any, broadcastList: object): ToReturnObject;
+    in(any): ToReturnObject;
+    simulate(event: Event): void;
+
+    public of(url: USVString): Server;
+  }
+
+  interface CloseOptions {
+    code: number;
+    reason: string;
+    wasClean: boolean;
+  }
+
+  interface EmitOptions {
+    websockets: WebSocket[];
+  }
+
+  interface ToReturnObject {
+    to: (chainedRoom: any, chainedBroadcaster: any) => ToReturnObject;
+    emit(event: Event, data: any): void;
+  }
+
+  interface ServerOptions {
+    verifyClient: () => any;
+    selectProtocol: () => any;
+  }
+}
