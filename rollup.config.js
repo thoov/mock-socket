87cc9eec3da6591174784eca9b51f4b5b1e361bc
Tomek Wytrębowicz <tomalecpub@gmail.com>
2019-09-19 07:06:35 +0200

    Export ES Module format with .mjs extension, (#276)
    as given lack of `"type": "module"` in `package.json`, and `.js` extension  Node tries to resolve `import "mock-socket/dist/mock-socket.es.js"` as CommonJS

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,7 @@ export default {
     { dest: 'dist/mock-socket.cjs.js', format: 'cjs' },
     { dest: 'dist/mock-socket.js', format: 'umd', moduleName: 'Mock' },
     { dest: 'dist/mock-socket.amd.js', format: 'amd', moduleName: 'Mock' },
-    { dest: 'dist/mock-socket.es.js', format: 'es' }
+    { dest: 'dist/mock-socket.es.js', format: 'es' },
+    { dest: 'dist/mock-socket.es.mjs', format: 'es' }
   ]
 };
