fcaabb2d84012ee4112a0508f449aa22585bc7c2
Pavel Birukov <pavel@birukov.me>
2019-12-24 09:48:42 +0200

    Trim query part from URL where it's necessary. Fixes #274 (#275)

--- tests/unit/network-bridge.test.js
+++ tests/unit/network-bridge.test.js
@@ -58,6 +58,17 @@ test('that attachWebSocket will add a websocket to the url map', t => {
   t.is(connection.websockets.length, 1, 'websocket property contains only the websocket object');
 });
 
+test('that attachWebSocket will add a websocket with query params to the url map', t => {
+  const resultServer = networkBridge.attachServer(fakeObject, 'ws://localhost:8080');
+  const resultWebSocket = networkBridge.attachWebSocket(fakeObject, 'ws://localhost:8080?foo=bar');
+  const connection = networkBridge.urlMap['ws://localhost:8080'];
+
+  t.deepEqual(resultServer, fakeObject, 'server returned because it was successfully added to the url map');
+  t.deepEqual(resultWebSocket, fakeObject, 'server returned as the websocket was successfully added to the map');
+  t.deepEqual(connection.websockets[0], fakeObject, 'fakeObject was added to the websockets array');
+  t.is(connection.websockets.length, 1, 'websocket property contains only the websocket object');
+});
+
 test('that attachWebSocket will add the same websocket only once', t => {
   const resultServer = networkBridge.attachServer(fakeObject, 'ws://localhost:8080');
   const resultWebSocket = networkBridge.attachWebSocket(fakeObject, 'ws://localhost:8080');
@@ -83,6 +94,18 @@ test('that server and websocket lookups return the correct objects', t => {
   t.deepEqual(websocketLookup.length, 1, 'the correct number of websockets are returned');
 });
 
+test('that server and websocket lookups ignore query params', t => {
+  networkBridge.attachServer(fakeObject, 'ws://localhost:8080');
+  networkBridge.attachWebSocket(fakeObject, 'ws://localhost:8080?foo=bar');
+
+  const serverLookup = networkBridge.serverLookup('ws://localhost:8080?foo1=1');
+  const websocketLookup = networkBridge.websocketsLookup('ws://localhost:8080?foo2=2');
+
+  t.deepEqual(serverLookup, fakeObject, 'server correctly returned');
+  t.deepEqual(websocketLookup, [fakeObject], 'websockets correctly returned');
+  t.deepEqual(websocketLookup.length, 1, 'the correct number of websockets are returned');
+});
+
 test('that removing server and websockets works correctly', t => {
   networkBridge.attachServer(fakeObject, 'ws://localhost:8080');
   networkBridge.attachWebSocket(fakeObject, 'ws://localhost:8080');
@@ -99,8 +122,46 @@ test('that removing server and websockets works correctly', t => {
   t.deepEqual(networkBridge.urlMap, {}, 'Url map is back in its default state');
 });
 
+test('that removing server and websockets works correctly with query params', t => {
+  networkBridge.attachServer(fakeObject, 'ws://localhost:8080');
+  networkBridge.attachWebSocket(fakeObject, 'ws://localhost:8080?foo=bar');
+
+  let websocketLookup = networkBridge.websocketsLookup('ws://localhost:8080?anything=else');
+  t.deepEqual(websocketLookup.length, 1, 'the correct number of websockets are returned');
+
+  networkBridge.removeWebSocket(fakeObject, 'ws://localhost:8080?arbitraryParameter');
+
+  websocketLookup = networkBridge.websocketsLookup('ws://localhost:8080?one=more');
+  t.deepEqual(websocketLookup.length, 0, 'the correct number of websockets are returned');
+
+  networkBridge.removeServer('ws://localhost:8080?please');
+  t.deepEqual(networkBridge.urlMap, {}, 'Url map is back in its default state');
+});
+
 test('a socket can join and leave a room', t => {
   const fakeSocket = { url: 'ws://roomy' };
+
+  networkBridge.attachServer(fakeObject, 'ws://roomy');
+  networkBridge.attachWebSocket(fakeSocket, 'ws://roomy');
+
+  let inRoom;
+  inRoom = networkBridge.websocketsLookup('ws://roomy', 'room');
+  t.is(inRoom.length, 0, 'there are no sockets in the room to start with');
+
+  networkBridge.addMembershipToRoom(fakeSocket, 'room');
+
+  inRoom = networkBridge.websocketsLookup('ws://roomy', 'room');
+  t.is(inRoom.length, 1, 'there is 1 socket in the room after joining');
+  t.deepEqual(inRoom[0], fakeSocket);
+
+  networkBridge.removeMembershipFromRoom(fakeSocket, 'room');
+
+  inRoom = networkBridge.websocketsLookup('ws://roomy', 'room');
+  t.is(inRoom.length, 0, 'there are no sockets in the room after leaving');
+});
+
+test('a socket with query params can join and leave a room', t => {
+  const fakeSocket = { url: 'ws://roomy?foo=bar' };
 
   networkBridge.attachServer(fakeObject, 'ws://roomy');
   networkBridge.attachWebSocket(fakeSocket, 'ws://roomy');
