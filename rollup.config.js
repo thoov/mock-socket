8230699205cc5f0bbb0844e7af672e7598ff83c2
Atrue <wtf.ozu@gmail.com>
2021-10-18 10:42:50 +0300

    Update rollup library

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
-import buble from 'rollup-plugin-buble';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import buble from '@rollup/plugin-buble';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  entry: 'src/index.js',
-  sourceMap: 'inline',
-  plugins: [buble(), resolve({ jsnext: true, main: true }), commonjs()],
-  targets: [
-    { dest: 'dist/mock-socket.cjs.js', format: 'cjs' },
-    { dest: 'dist/mock-socket.js', format: 'umd', moduleName: 'Mock' },
-    { dest: 'dist/mock-socket.amd.js', format: 'amd', moduleName: 'Mock' },
-    { dest: 'dist/mock-socket.es.js', format: 'es' },
-    { dest: 'dist/mock-socket.es.mjs', format: 'es' }
+  input: 'src/index.js',
+  plugins: [buble(), nodeResolve(), commonjs()],
+  output: [
+    { file: 'dist/mock-socket.cjs.js', format: 'cjs' },
+    { file: 'dist/mock-socket.js', format: 'umd', name: 'Mock' },
+    { file: 'dist/mock-socket.amd.js', format: 'amd', name: 'Mock' },
+    { file: 'dist/mock-socket.es.js', format: 'es' },
+    { file: 'dist/mock-socket.es.mjs', format: 'es' }
   ]
 };
