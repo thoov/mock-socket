1476a9898b9338765648d4a4f90287aea021d275
Yann Briançon <ybr@outsight.tech>
2022-08-12 12:58:12 +0200

    Handle Buffer message data

--- src/helpers/normalize-send.js
+++ src/helpers/normalize-send.js
@@ -1,5 +1,7 @@
 export default function normalizeSendData(data) {
-  if (Object.prototype.toString.call(data) !== '[object Blob]' && !(data instanceof ArrayBuffer)) {
+  if (data instanceof Buffer) {
+    data = data.buffer;
+  } else if (Object.prototype.toString.call(data) !== '[object Blob]' && !(data instanceof ArrayBuffer)) {
     data = String(data);
   }
 
