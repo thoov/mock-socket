bd771f0d2505fb4debec0f09b3749de6e7e2ecba
artin <artin@cat.ms>
2023-12-18 22:12:38 +0800

    Disable convert data to string

--- src/helpers/normalize-send.js
+++ src/helpers/normalize-send.js
@@ -1,7 +1,3 @@
 export default function normalizeSendData(data) {
-  if (Object.prototype.toString.call(data) !== '[object Blob]' && !(data instanceof ArrayBuffer)) {
-    data = String(data);
-  }
-
   return data;
 }
